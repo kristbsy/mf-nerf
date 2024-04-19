309e4ffc47fefe44723122b84dc90e2b961cc259
Tommy <tommydl@stud.ntnu.no>
2024-04-19 14:21:37 +0200

    update segmentation

--- seg/process.py
+++ seg/process.py
@@ -1,3 +1,4 @@
+from tqdm import tqdm
 from transformers import pipeline, utils
 from PIL import Image
 import requests
@@ -6,6 +7,7 @@ from pathlib import Path
 import argparse
 
 utils.logging.set_verbosity_error()
+semantic_segmentation = pipeline(task="image-segmentation", model="nvidia/segformer-b1-finetuned-cityscapes-1024-1024", feature_extractor="nvidia/segformer-b1-finetuned-cityscapes-1024-1024", device=0)
 
 # Define the class names and color palette from Cityscapes
 CLASSES = [
@@ -24,9 +26,9 @@ FILTER = ['car', 'truck', 'bus', 'train', 'motorcycle', 'bicycle', 'person', 'ri
 def load_image(image_path: str):
     """Load an image from a file or URL."""
     if image_path.startswith('http://') or image_path.startswith('https://'):
-        return Image.open(requests.get(image_path, stream=True).raw).convert('RGB')
+        return Image.open(requests.get(image_path, stream=True).raw).convert('RGBA')
     else:
-        return Image.open(image_path).convert('RGB')
+        return Image.open(image_path).convert('RGBA')
     
 
 def segment_image(image_path: str):
@@ -34,7 +36,6 @@ def segment_image(image_path: str):
     image = load_image(image_path)
     original_size = image.size
 
-    semantic_segmentation = pipeline("image-segmentation", "nvidia/segformer-b1-finetuned-cityscapes-1024-1024", feature_extractor="nvidia/segformer-b1-finetuned-cityscapes-1024-1024")
     results = semantic_segmentation(image)
     mask = np.zeros((original_size[1], original_size[0]), dtype=bool)
     for r in results:
@@ -43,8 +44,7 @@ def segment_image(image_path: str):
 
     # Apply transparency based on segmentation
     alpha_channel = np.where(mask, 0, 255).astype(np.uint8)  # Set alpha to 0 for masked areas
-    image_rgba = image.convert("RGBA")
-    image_array = np.array(image_rgba)
+    image_array = np.array(image)
     image_array[:, :, 3] = alpha_channel
     
     # Save the modified image inplace
@@ -56,8 +56,10 @@ def main():
     parser = argparse.ArgumentParser(description='Segment images')
     parser.add_argument('data', type=str, help='Folder containing images to segment')
     data_folder = Path(parser.parse_args().data)
+    pathlist = list(data_folder.glob('*.png'))
     
-    for image_path in data_folder.glob('*.png'):
+    ## use tqdm to show progress bar
+    for image_path in tqdm(pathlist):
         segment_image(image_path.as_posix())
 
 
