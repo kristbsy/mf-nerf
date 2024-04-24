defc07da6002350e5c54de35049d5fa8e3fb83a6
Tommy <tommydl@stud.ntnu.no>
2024-04-24 12:04:19 +0200

    segmentation filter

--- seg/process.py
+++ seg/process.py
@@ -22,8 +22,8 @@ PALETTE = np.array([[128, 64, 128], [244, 35, 232], [70, 70, 70], [102, 102, 156
                     [70, 130, 180], [220, 20, 60], [255, 0, 0], [0, 0, 142], [0, 0, 70], [0, 60, 100], 
                     [0, 80, 100], [0, 0, 230], [119, 11, 32]])
 
+#FILTER = ["person"]
 FILTER = ['car', 'truck', 'bus', 'train', 'motorcycle', 'bicycle', 'person', 'rider']
-
 
 def load_image(image_path: str):
     """Load an image from a file or URL."""
